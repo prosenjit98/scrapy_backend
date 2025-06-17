285606c063da55478513d3076606311eec0fa43c
Prosenjit Chongder <prosenjit.chongder@geogo.in>
2025-06-17 21:40:26 +0530

    fixed

--- database/migrations/1749998929131_alter_vehicles_table.ts
+++ database/migrations/1749998929131_alter_vehicles_table.ts
@@ -3,6 +3,7 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 export default class extends BaseSchema {
   protected tableName_1 = 'vehicle_makes'
   protected tableName_2 = 'vehicle_models'
+  protected tableName_3 = 'users'
 
   async up() {
     this.schema.alterTable('parts', (table) => {
@@ -20,6 +21,13 @@ export default class extends BaseSchema {
         .references('id')
         .inTable(this.tableName_2)
         .onDelete('SET NULL')
+      table
+        .integer('vendor_id')
+        .unsigned()
+        .nullable()
+        .references('id')
+        .inTable(this.tableName_3)
+        .onDelete('SET NULL')
     })
     this.schema.table('vehicles', (table) => {
       table
@@ -36,8 +44,8 @@ export default class extends BaseSchema {
         .references('id')
         .inTable(this.tableName_2)
         .onDelete('SET NULL')
-      table.dropColumn('make')
-      table.dropColumn('model')
+      // table.dropColumn('make')
+      // table.dropColumn('model')
     })
   }
 
@@ -45,6 +53,7 @@ export default class extends BaseSchema {
     this.schema.alterTable('parts', (table) => {
       table.dropColumn('vehicle_make_id')
       table.dropColumn('vehicle_model_id')
+      table.dropColumn('vendor_id')
     })
     this.schema.alterTable('vehicles', (table) => {
       table.dropColumn('vehicle_make_id')
